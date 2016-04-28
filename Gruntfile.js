e97be899f784b2cf7814eaf8534ad6cba670e0ea
Bishal Napit <mebishalnapit@gmail.com>
2016-04-28 15:44:01 +0545

    Integrated grunt task runner in the theme

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+/* jshint node:true */
+module.exports = function( grunt ){
+	'use strict';
+
+	grunt.initConfig({
+
+		// Setting folder templates.
+		dirs: {
+			js: 'js',
+			css: 'css'
+		},
+
+		// JavaScript linting with JSHint.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/*.js',
+				'!<%= dirs.js %>/*.min.js',
+            '!<%= dirs.js %>/jquery.bxslider/jquery.bxslider.js',
+            '!<%= dirs.js %>/magnific-popup/jquery.magnific-popup.js',
+            '!<%= dirs.js %>/color-picker.js',
+            '!<%= dirs.js %>/html5shiv.js',
+            '!<%= dirs.js %>/image-uploader.js',
+            '!<%= dirs.js %>/jquery.nav.js',
+            '!<%= dirs.js %>/jquery.parallax-1.1.3.js',
+            '!<%= dirs.js %>/metabox-toggle.js'
+			]
+		},
+
+		// Generate POT files.
+		makepot: {
+			options: {
+				type: 'wp-theme',
+				domainPath: 'languages',
+				potHeaders: {
+					'report-msgid-bugs-to': 'themegrill@gmail.com',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+				}
+			},
+			dist: {
+				options: {
+					potFilename: 'himalayas.pot',
+					exclude: [
+						'deploy/.*' // Exclude deploy directory
+					]
+				}
+			}
+		},
+
+		// Check textdomain errors.
+		checktextdomain: {
+			options: {
+				text_domain: 'himalayas',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src: [
+					'**/*.php',
+					'!node_modules/**'
+				],
+				expand: true
+			}
+		}
+
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'jshint'
+	]);
+
+	grunt.registerTask( 'dev', [
+		'default',
+		'makepot'
+	]);
+};
