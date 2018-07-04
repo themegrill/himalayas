aa52153748cf568d986804d1b26b2a5a0c46ecf6
Nitu Shrestha <shresthanitu984@gmail.com>
2018-07-04 11:19:05 +0545

    Fix - Header image size

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -2,9 +2,9 @@
 /**
  * Himalayas Theme Customizer
  *
- * @package ThemeGrill
+ * @package    ThemeGrill
  * @subpackage Himalayas
- * @since Himalayas 1.0.9
+ * @since      Himalayas 1.0.9
  */
 
 /**
@@ -13,14 +13,15 @@
 function himalayas_custom_header_setup() {
 	// Add Image Headers / Video Headers in 4.7
 	add_theme_support( 'custom-header', array(
-		'width'                => 2000,
-		'height'               => 400,
-		'flex-height'          => true,
-		'header-text'          => true,
-		'video'                => true,
-		'header-text'          => false,
+		'width'       => 2000,
+		'height'      => 400,
+		'flex-height' => true,
+		'flex-width'  => true,
+		'video'       => true,
+		'header-text' => false,
 	) );
 }
+
 add_action( 'after_setup_theme', 'himalayas_custom_header_setup' );
 
 // Filter the get_header_image_tag() for option of adding the link back to home page option
@@ -30,8 +31,8 @@ function himalayas_header_image_markup( $html, $header, $attr ) {
 	if ( ( get_theme_mod( 'himalayas_slide_on_off', '' ) == 0 ) || ( ( get_theme_mod( 'himalayas_slide_on_off', '' ) == 1 ) && ! is_front_page() ) ) {
 		$header_image = get_header_image();
 
-		if( ! empty( $header_image ) ) {
-			$output .= '<div class="header-image-wrap"><img src="' . esc_url( $header_image ) . '" class="header-image" width="' . get_custom_header()->width . '" height="' .  get_custom_header()->height . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '"></div>';
+		if ( ! empty( $header_image ) ) {
+			$output .= '<div class="header-image-wrap"><img src="' . esc_url( $header_image ) . '" class="header-image" width="' . get_custom_header()->width . '" height="' . get_custom_header()->height . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '"></div>';
 		}
 	}
 
@@ -41,4 +42,5 @@ function himalayas_header_image_markup( $html, $header, $attr ) {
 function himalayas_header_image_markup_filter() {
 	add_filter( 'get_header_image_tag', 'himalayas_header_image_markup', 10, 3 );
 }
-add_action( 'himalayas_header_image_markup_render','himalayas_header_image_markup_filter' );
+
+add_action( 'himalayas_header_image_markup_render', 'himalayas_header_image_markup_filter' );
