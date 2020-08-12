a792728411fb212b34fec32b6f9e5bf0818b14d6
Premi Jimee <hype.zambi@gmail.com>
2020-08-12 16:07:46 +0545

    Remove - TDI notice

--- functions.php
+++ functions.php
@@ -155,7 +155,6 @@ define( 'HIMALAYAS_THEME_VERSION', $himalayas_theme->get( 'Version' ) );
  */
 if ( is_admin() ) {
 	require get_template_directory() . '/inc/admin/class-himalayas-notice.php';
-	require get_template_directory() . '/inc/admin/class-himalayas-tdi-notice.php';
 	require get_template_directory() . '/inc/admin/class-himalayas-admin.php';
 	require get_template_directory() . '/inc/admin/class-himalayas-welcome-notice.php';
 	require get_template_directory() . '/inc/admin/class-himalayas-upgrade-notice.php';
