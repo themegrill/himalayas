eab691a202ad032be4911d966ce48352a8ffd7bc
Premiii <jimeepremi@gmail.com>
2021-05-11 20:55:31 +0545

    Update - Discount Coupon

--- inc/admin/class-himalayas-upgrade-notice.php
+++ inc/admin/class-himalayas-upgrade-notice.php
@@ -78,8 +78,8 @@ class Himalayas_Upgrade_Notice extends Himalayas_Notice {
 					),
 					'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
 					'Himalayas',
-					'<code class="coupon-code">upgrade15</code>',
-					'15%'
+					'<code class="coupon-code">TGFREEUSER</code>',
+					'25%'
 				);
 				?>
 			</p>
