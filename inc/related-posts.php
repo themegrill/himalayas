b4c0ee00e7e542178f2b412c320caec37e7639f6
somthemegrill <som.shrestha@themegrill.com>
2024-04-01 17:17:03 +0545

    fix escaping issue

--- inc/related-posts.php
+++ inc/related-posts.php
@@ -54,7 +54,7 @@
 								$time_string
 							); ?>
 
-							<span class="byline author vcard"><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo get_the_author(); ?>"><?php echo esc_html( get_the_author() ); ?></a></span>
+							<span class="byline author vcard"><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>"><?php echo esc_html( get_the_author() ); ?></a></span>
 						</div>
 
 					</div>
